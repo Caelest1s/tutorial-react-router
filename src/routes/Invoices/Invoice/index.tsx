e7cba7206837fc918cb90601f88908ae16d6c048
Jefferson Celestino <jefferson.ucb02@gmail.com>
2024-03-19 19:28:40 -0300

    Navigating Programmatically

--- src/routes/Invoices/Invoice/index.tsx
+++ src/routes/Invoices/Invoice/index.tsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router";
-import { getInvoice } from "../../../data";
+import { useNavigate } from "react-router-dom";
+import { deleteInvoice, getInvoice } from "../../../data";
 
 export default function Invoice() {
+
     const params = useParams();
+    const navigate = useNavigate();
 
     const invoice = getInvoice(Number(params.invoiceId));
 
@@ -16,6 +19,16 @@ export default function Invoice() {
                         {invoice.name}: {invoice.number}
                     </p>
                     <p>Due Date: {invoice.due}</p>
+                    <p>
+                        <button
+                            onClick={() => {
+                                deleteInvoice(invoice.number);
+                                navigate("/invoices" + location.search);
+                            }}
+                        >
+                            Delete
+                        </button>
+                    </p>
                 </main>
             }
         </>
